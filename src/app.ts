7da5f0deb400c826e2b0cadd6ba35d083fb2f5fb
Shirley Bailey <shirleybailey1002@outlook.com>
2026-04-02 21:29:30 +0300

    fix: add type definitions for cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import { authMiddleware } from './middlewares/auth'
 import { usageRouter } from './modules/usage/usage.controller'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(authMiddleware)
 
